e68e201434bdccccd17e20fed501b88c85c5686d
RGV <ricardo.guzman.velasco@outlook.com>
2021-08-08 19:17:58 +0200

    Docs

---
+++ Tests/Editor/Domain/SlottableInventoryTests.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace Kalendra.Inventory.Tests.Editor.Domain
+{
+    public class SlottableInventoryTests
+    {
+        [Test]
+        public void METHOD()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
